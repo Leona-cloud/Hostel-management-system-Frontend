ad46b42c0d612eebb365160531394508904b9756
Leona-cloud <chisomokafor038@gmail.com>
2023-07-22 09:41:40 +0100

    fix Json error

--- hostel-mgt/src/components/RegisterCont1.jsx
+++ hostel-mgt/src/components/RegisterCont1.jsx
@@ -37,8 +37,8 @@ const RegisterCont1 = () => {
         console.log(resp)
         const url =  "https://hostel-mgt.onrender.com/api/auth/student/update-details"
         const {data: res} = await axiosInstance.post(url, {...data, studentImage:resp.data.url});
-        console.log(res.data.data.student, 'response')
-        localStorage.setItem("student",res.data.data.student)
+        console.log(res.data.student, 'response')
+        localStorage.setItem("student", JSON.stringify(res.data.student))
         toast({
           title: 'Success',
           description:"Upload successful" ,
@@ -62,17 +62,16 @@ const RegisterCont1 = () => {
       }));
   };
 
-  
 
-  useEffect(() => {
+  // useEffect(() => {
    
-    if (success) {
-      const timer = setTimeout(() => {
-        navigate("/setup-hostel");
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [success]);
+  //   if (success) {
+  //     const timer = setTimeout(() => {
+  //       navigate("/setup-hostel");
+  //     }, 3000);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [success]);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2  w-full bg-slate-300">
